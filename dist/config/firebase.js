afdc9ab6cc71336bc99f281b9a076cee6a8cbc29
OctoLuis <louiskano.work@gmail.com>
2023-08-09 12:28:38 -0600

    update gitignore+refactoring+working on messaging

---
+++ dist/config/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import 'dotenv/config';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: process.env.firebase_apikey,
+    authDomain: process.env.firebase_authDomain,
+    projectId: process.env.firebase_projectId,
+    storageBucket: process.env.firebase_storageBucket,
+    messagingSenderId: process.env.firebase_messagingSenderId,
+    appId: process.env.firebase_appId,
+    measurementId: process.env.firebase_measurementId,
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+//# sourceMappingURL=firebase.js.map
