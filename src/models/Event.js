1667c9ea372d564e9378bc08fd5f2cbede1ddf4e
mingogog <mingogog@gmail.com>
2023-08-09 14:45:36 -0400

    setup models

---
+++ src/models/Event.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const { Schema, ObjectId } = mongoose;
+
+const eventSchema = new Schema({
+  id: Number,
+  category: {
+    type: String,
+    enum : ["A", "B", "C"],
+    default: ''
+  },
+  createdAt: Date,
+  updatedAt: Date,
+  location: String,
+  lag: String,
+  lng: String,
+  description: String,
+  posterJson: String,
+  ranking: Number,
+  userId:{
+    type: ObjectId,
+    ref: "User"
+  }
+},{ collection: 'events' });
+
+module.exports = mongoose.model('Event', eventSchema)
