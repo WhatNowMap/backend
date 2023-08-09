d9ebbd4d90cef35cfa7bc6bf7cf87c51a1f7d003
OctoLuis <louiskano.work@gmail.com>
2023-08-09 11:39:45 -0600

    env firebase

--- src/models/Notification.ts
+++ src/models/Notification.ts
@@ -1,19 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import 'dotenv/config'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDN3fm1Kz8XMvYiqYnNcd9G48lMHhQj5fY",
-  authDomain: "whatnow-e471a.firebaseapp.com",
-  projectId: "whatnow-e471a",
-  storageBucket: "whatnow-e471a.appspot.com",
-  messagingSenderId: "320190413317",
-  appId: "1:320190413317:web:55170c4bc25739cc99fa6c",
-  measurementId: "G-MBMFZCNHPW"
+  apiKey: process.env.firebase_apikey,
+  authDomain: process.env.firebase_authDomain,
+  projectId: process.env.firebase_projectId,
+  storageBucket: process.env.firebase_storageBucket,
+  messagingSenderId: process.env.firebase_messagingSenderId,
+  appId: process.env.firebase_appId,
+  measurementId: process.env.firebase_measurementId,
 };
 
 // Initialize Firebase
