0ca136da524856b339ab30a0b621509a8936b0d2
Karl-Hui <KarlHui0311@gmail.com>
2023-08-10 23:59:16 -0400

    add: added facebook auth prototype

--- index.ts
+++ index.ts
@@ -1,18 +1,69 @@
-import express from "express";
+import express from 'express';
 const app: any = express();
-const { connectToMongo } = require('./src/config/mongoose.ts')
+const { connectToMongo } = require('./src/config/mongoose.ts');
+const session = require('express-session');
+const passport = require('./src/controllers/facebook-auth-controller');
 
 // Router
-const { eventRouter, reportRouter } = require("./src/routes");
+const {
+  eventRouter,
+  reportRouter,
+  faceBookAuthRouter,
+} = require('./src/routes');
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user);
+});
+passport.deserializeUser(function (obj, cb) {
+  cb(null, obj);
+});
 
 // Post Request
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// Routing Control
-app.use("/event", eventRouter);
-app.use("/report", reportRouter);
+app.use(
+  session({
+    secret: 'your-secret-key', // Change this to your preferred secret key
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-app.listen(8080, () => {
-  console.log("The server is running on 8080");
+// Routing Control
+app.get('/', (req, res) => {
+  res.send('Welcome!');
+});
+
+app.use('/event', eventRouter);
+app.use('/report', reportRouter);
+app.use('/auth/facebook', faceBookAuthRouter);
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, async () => {
+  console.log('The server is running on ' + port);
   connectToMongo();
-})
+});
+
+//  For Test
+// const { User } = require('./src/models');
+
+// const updateDB = async () => {
+//   const user = await User.create({
+//     userId: 1,
+//     nameName: '8798789',
+//     password: '3',
+//     email: '4',
+//     fixedLocation: '5',
+//     tag: ['A', 'B'],
+//     provider: 'facebook',
+//   });
+
+//   console.log(user);
+// };
+
+// app.listen(8080, async () => {
+//   console.log('The server is running on 8080');
+//   connectToMongo();
+//   await updateDB();
+// });
