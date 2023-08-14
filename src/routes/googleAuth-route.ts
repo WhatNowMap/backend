dfc80eecec02d36704da4869a49e0ed10578ebd3
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-08-14 16:06:31 -0400

    feat: google oauth intergration with frontend

--- src/routes/googleAuth-route.ts
+++ src/routes/googleAuth-route.ts
@@ -5,7 +5,6 @@ const { isLoggedIn } = require('../middlewares/auth-middleware')
 const { dirname } = require('path');
 const appDir = dirname(require.main.filename);
 
-<<<<<<< HEAD
 const successRoute = process.env.FRONTEND_PORT
   ? `${process.env.FRONTEND_PORT}/list`
   : '/auth/google/success'
@@ -13,39 +12,16 @@ const failureRoute = process.env.FRONTEND_PORT
   ? `${process.env.FRONTEND_PORT}/`
   : '/auth/google/failure'
 
-router.get('/', (req, res)=>{
-    res.sendFile(appDir + '/index.html')
-})
-router.get('/auth/google',
-  passport.authenticate('google', { scope:
-    [ 'email', 'profile' ] }
-  )
-);
-router.get('/auth/google/callback',
-    passport.authenticate( 'google', {
-      successRedirect: successRoute,
-      failureRedirect: failureRoute
-    })
-);
-router.get('/auth/google/failure', (req, res)=>{
-    res.send("Something went wrong!")
-});
-router.get('/auth/protected', isLoggedIn, (req, res)=>{
-    let name = req.user.userName;
-    res.send(`Hello ${name}`);
-});
-=======
 router.get('/test', (req, res)=>{
   res.sendFile(appDir + '/index.html')
-})
->>>>>>> f68ce671e96d1d661eb15cd5889a1613d8d85f3b
+});
 
 router.get('/', passport.authenticate('google', { scope: [ 'email', 'profile' ]}));
 
 router.get('/callback',
   passport.authenticate( 'google', {
-    successRedirect: '/auth/google/success',
-    failureRedirect: '/auth/google/failure'
+    successRedirect: successRoute,
+    failureRedirect: failureRoute,
   }),
   googleAuthController.loginSuccessCallback
 );
