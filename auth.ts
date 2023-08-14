384c853072ff2f1c6c6edcb1e3877dd65733cab0
justynleung <justyn@Justyns-MBP.ht.home>
2023-08-14 15:21:54 -0400

    debug

--- auth.ts
+++ auth.ts
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const User = require('./src/models/User')
-require('dotenv').config()
+require("dotenv").config()
 
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
--- src/routes/googleAuth-route.ts
+++ src/routes/googleAuth-route.ts
@@ -3,27 +3,30 @@ const passport = require('passport');
 const { isLoggedIn } = require('../middlewares/auth-middleware')
 const { dirname } = require('path');
 const appDir = dirname(require.main.filename);
+require("dotenv").config()
 
 // router.get('/', (req, res)=>{
 //     res.sendFile(appDir + '/index.html')
 // })
 router.get('/',
-  passport.authenticate('google', { scope:
-    [ 'email', 'profile' ] }
+  passport.authenticate('google', {
+    scope:
+      ['email', 'profile']
+  }
   )
 );
 router.get('/callback',
-    passport.authenticate( 'google', {
-      successRedirect: '/auth/google/success',
-      failureRedirect: '/auth/google/failure'
-    })
+  passport.authenticate('google', {
+    successRedirect: '/auth/google/success',
+    failureRedirect: '/auth/google/failure'
+  })
 );
-router.get('/failure', (req, res)=>{
-    res.send("Something went wrong!")
+router.get('/failure', (req, res) => {
+  res.send("Something went wrong!")
 });
-router.get('/success', isLoggedIn, (req, res)=>{
-    let name = req.user.userName;
-    res.send(`Hello ${name}`);
+router.get('/success', isLoggedIn, (req, res) => {
+  let name = req.user.userName;
+  res.send(`Hello ${name}`);
 });
 
 module.exports = router;
