b2cc1125e065003818d967583645b5842ae35a5f
OctoLuis <louiskano.work@gmail.com>
2023-08-18 10:01:13 -0600

    Refactoring: Sending Push Notifications. Todo: Front End Navigator Service Worker Set Up

--- src/controllers/notifications/notification-controller.ts
+++ src/controllers/notifications/notification-controller.ts
@@ -1,18 +1,30 @@
 import { CloudMessage, sendMessage } from "./admin-send-message";
+import {
+  initializeCloudMessaging,
+  requestPermission,
+  receiveMessage,
+} from "./cloud-messaging";
 
 exports.notify = async function (req, res) {
   try {
-    const message: CloudMessage = {
+    const cloudMessage: CloudMessage = {
       notification: {
         title: "WhatNow: New Event Posted",
         body: "An event is happening around you. Check it now.",
       },
       topic: "all_user",
     };
-    sendMessage(message);
-    res.status(200).send({ message: "Notify Successfully." });
+    sendMessage(cloudMessage);
+    res.status(200).send({ message: "Notify Successfully.", cloudMessage });
+
+    // Receiving Test:
+    // const messaging = await initializeCloudMessaging();
+    // // requestPermission();
+    // receiveMessage(messaging);
   } catch (err) {
     console.log(err);
     res.status(500).send(err);
   }
 };
+
+exports.receive = async function (req, res) {};
