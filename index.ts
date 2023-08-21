4e6b7019b6f53064d0a65ba9c189c446b2370588
mingogog <mingogog@gmail.com>
2023-08-21 14:52:59 -0400

    set req header in index.ts

--- index.ts
+++ index.ts
@@ -38,6 +38,14 @@ app.use(expressSession);
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 const {
   facebookAuthController,
   twitterAuthController,
